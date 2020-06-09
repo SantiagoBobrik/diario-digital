143df656d74b7ee2766d93da2ca03dd6f5513096
SantiagoBobrik <santiago.bobrik@gmail.com>
2020-06-09 03:39:44 +0000

    cache

---
+++ storage/framework/views/219dc15a98c78a3a2df3208ea03c10187e0f4c6d.php
@@ -0,0 +1,258 @@
+<?php $__env->startSection('top'); ?>
+
+
+
+
+<?php if(isset($horizontalAds[0]) == 1): ?>
+
+
+<div class="section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 section-row text-center">
+				<a target="_blank" href="<?php echo e($horizontalAds[0]->url); ?>" style="display: inline-block;margin: auto;">
+					<img style="width: 728px; height: 90px" class="img-responsive"
+						src="<?php echo e(url('storage/'.$horizontalAds[0]->image)); ?>" alt="">
+				</a>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+<?php endif; ?>
+
+
+
+
+<!-- SECTION POST TOP  -->
+
+<div class="section">
+
+	<div class="container">
+
+		<div class="row hot-post">
+			<?php $__currentLoopData = $postsTrend; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+			<?php if($post->trend == 1): ?>
+			<div class="col-md-8 hot-post-left mt-3">
+
+				<div class="post post-thumb">
+					<a href="/noticia/<?php echo e($post->id); ?>" class="post-img trend-left"><img src="storage/<?php echo e($post->image); ?>"
+							alt=""></a>
+					<div class="post-body">
+						<div class="post-category">
+							<a href="seccion/<?php echo e($post->getCategory->slug); ?>"><?php echo e($post->getCategory->name); ?></a>
+						</div>
+						<h3 class="post-title title-trend"><a href="/noticia/<?php echo e($post->id); ?>"><?php echo e($post->tittle); ?></a></h3>
+
+					</div>
+				</div>
+			</div>
+			<?php endif; ?>
+
+			<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+
+
+			<div class="col-md-4 hot-post-right">
+				<?php $__currentLoopData = $postsTrend; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+				<?php if($post->trend == 2): ?>
+
+				<div class="post post-thumb">
+					<a class="post-img trend-right" href="/noticia/<?php echo e($post->id); ?>"><img class="img-fluid"
+							src="storage/<?php echo e($post->image); ?>" alt=""></a>
+					<div class="post-body">
+						<div class="post-category">
+							<a href="seccion/<?php echo e($post->getCategory->slug); ?>"><?php echo e($post->getCategory->name); ?></a>
+						</div>
+						<h3 class="post-title"><a href="/noticia/<?php echo e($post->id); ?>"><?php echo e($post->tittle); ?></a></h3>
+					</div>
+				</div>
+				<?php endif; ?>
+
+				<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+
+
+				<?php $__currentLoopData = $postsTrend; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+				<?php if($post->trend == 3): ?>
+
+
+				<div class="post post-thumb">
+					<a class="post-img trend-right" href="/noticia/<?php echo e($post->id); ?>"><img src=" storage/<?php echo e($post->image); ?>"
+							alt=""></a>
+					<div class="post-body">
+						<div class="post-category">
+							<a href="seccion/<?php echo e($post->getCategory->slug); ?>"><?php echo e($post->getCategory->name); ?></a>
+
+						</div>
+						<h3 class="post-title"><a href="/noticia/<?php echo e($post->id); ?>"><?php echo e($post->tittle); ?></a></h3>
+					</div>
+				</div>
+
+				<?php endif; ?>
+
+				<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+			</div>
+		</div>
+
+	</div>
+
+</div>
+
+<!-- ---------------------------- END TOP SECTION  --------------------------- -->
+
+
+
+<?php if(isset($horizontalAds[1]) == 1): ?>
+
+<div class="section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 section-row text-center">
+				<a target="_blank" href="<?php echo e($horizontalAds[1]->url); ?>" style="display: inline-block;margin: auto;">
+					<img style="width: 728px; height: 90px" class="img-responsive"
+						src="<?php echo e(url('storage/'.$horizontalAds[1]->image)); ?>" alt="">
+				</a>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+<?php endif; ?>
+
+
+
+<div class="section">
+	<!-- container -->
+	<div class="container">
+		<!-- row -->
+		<div class="row">
+			<div class="col-md-8">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+						<div class="section-title">
+							<h2 class="title">Noticias Recientes</h2>
+
+						</div>
+					</div>
+					<?php $__currentLoopData = $recents; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $recent): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+					<div class="col-md-6">
+						<div class="post">
+
+							<a style="heigth:100px" class="post-img" href="/noticia/<?php echo e($recent->id); ?>"><img class="pepe"
+									src="storage/<?php echo e($recent->image); ?>" alt=""></a>
+
+
+							<div class="post-body">
+								<div class="post-category">
+									<a href="/seccion/<?php echo e($recent->getCategory->slug); ?>"><?php echo e($recent->getCategory->name); ?></a>
+								</div>
+								<h3 class="post-title"><a href="/noticia/<?php echo e($recent->id); ?>"><?php echo e($recent->tittle); ?></a></h3>
+
+							</div>
+						</div>
+					</div>
+					<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+				</div>
+
+
+				<div class="row">
+					<?php $__currentLoopData = $categories; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $category): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+					<div class="col-md-12">
+						<div class="section-title">
+							<h2 class="title"><?php echo e($category->name); ?></h2>
+						</div>
+					</div>
+					<!-- post -->
+
+
+					<?php $__currentLoopData = $posts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+					<?php if($post->category_id == $category->id): ?>
+
+					<div class="col-md-6">
+
+						<div class="post post-sm">
+							<a class="post-img" href="/noticia/<?php echo e($post->id); ?>"><img src="storage/<?php echo e($post->image); ?>"
+									alt=""></a>
+							<div class="post-body">
+								<div class="post-category">
+									<a href="seccion/<?php echo e($category->slug); ?>"><?php echo e($post->getCategory->name); ?></a>
+								</div>
+								<h3 class="post-title title-sm"><a href="/noticia/<?php echo e($post->id); ?>"><?php echo e($post->tittle); ?></a>
+								</h3>
+							</div>
+						</div>
+					</div>
+					<?php endif; ?>
+
+
+
+
+
+
+					<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+					<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+
+
+
+				</div>
+
+			</div>
+
+			
+			<div class="col-md-4">
+
+
+				<?php $__currentLoopData = $normalAds; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $ad): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+
+				<div class="aside-widget text-center col-12">
+
+					<a target="_blank" href="<?php echo e($ad->url); ?>" style="display: inline-block;margin: auto;">
+						<img class="img-responsive" src="storage/<?php echo e($ad->image); ?>" alt="">
+
+					</a>
+				</div>
+
+				<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+
+			</div>
+
+		</div>
+	</div>
+	<!-- /row -->
+</div>
+<!-- /container -->
+</div>
+
+
+
+
+
+<?php if(isset($horizontalAds[2]) == 1): ?>
+
+<div class="section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 section-row text-center">
+				<a target="_blank" href="<?php echo e($horizontalAds[2]->url); ?>" style="display: inline-block;margin: auto;">
+					<img style="width: 728px; height: 90px" class="img-responsive"
+						src="<?php echo e(url('storage/'.$horizontalAds[2]->image)); ?>" alt="">
+				</a>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+<?php endif; ?>
+
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.navbar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/resources/views/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/871a9489de7db4fe97177d5714199de8e6772ed9.php
@@ -0,0 +1,171 @@
+<meta property="og:title" content="<?php echo e($post->tittle); ?>">
+<meta property="og:image" content="<?php echo e(url('storage/'.$post->image)); ?>">
+<meta property="og:url" content="http://lainformacioncanuelas.com/noticia/<?php echo e($post->id); ?>">
+<?php $__env->startSection('top'); ?>
+
+<style>
+    .titulo {
+        font-size: 45px
+    }
+
+
+    .image {
+        width: 60%
+    }
+
+
+
+    .divider:after {
+        content: "";
+        display: inline-block;
+        height: 2px;
+        background-color: #e8eaed;
+        position: absolute;
+        left: 0px;
+        right: 0px;
+        bottom: -10px;
+        z-index: 10;
+
+    }
+
+    @media  only screen and (max-width: 767px) {
+        #post-header {
+            padding-top: 50px;
+            padding-bottom: 80px;
+        }
+
+        .titulo {
+            font-size: 30px
+        }
+
+        .image {
+            width: 100%
+        }
+
+    }
+</style>
+
+
+
+<!-- PAGE HEADER -->
+
+
+<!-- /PAGE HEADER -->
+
+<!-- /HEADER -->
+
+
+<?php if(isset($horizontalAds[0]) == 1): ?>
+
+
+<div class="section">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-12 section-row text-center">
+                <a target="_blank" href="<?php echo e($horizontalAds[0]->url); ?>" style="display: inline-block;margin: auto;">
+                    <img style="width: 728px; height: 90px" class="img-responsive"
+                        src="<?php echo e(url('storage/'.$horizontalAds[0]->image)); ?>" alt="">
+                </a>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<?php endif; ?>
+
+<!-- section -->
+<div class="section">
+    <!-- container -->
+    <div class="container">
+        <!-- row -->
+        <div class="row">
+            <div class="col-md-12">
+
+                <div class="col-md-12 col-lg-12 col-sm-12 col-12 tittle mb-2 mt-4 ">
+
+                    <h1 class=" titulo "><?php echo e($post->tittle); ?></h1>
+
+                </div>
+
+                <div class="col-md-12 col-lg-12 col-sm-12 col-12 tittle mb-5 ">
+                    <p class="divider p-0 mt-0"><?php echo $post->description; ?></p>
+                </div>
+
+                <div class="col-md-12 col-lg-12 col-sm-12 col-12 tittle mt-4 mb-5 text-center ">
+
+                    <img class="image img-fluid" src="<?php echo e(url('storage/'.$post->image)); ?>" alt="">
+
+                </div>
+
+
+
+                <div class="col-md-8 col-lg-8 col-sm-12 col-12 mt-4 ">
+                    <?php echo $post->content; ?>
+
+                </div>
+
+                
+                <div class="col-md-4 col-lg-4 col-sm-12  col-12">
+
+
+                    <?php $__currentLoopData = $normalAds; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $ad): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+
+                    <div class="aside-widget text-center col-12">
+
+                        <a target="_blank" href="<?php echo e($ad->url); ?>" style="display: inline-block;margin: auto;">
+                            <img class="img-responsive" src="<?php echo e(url('storage/'.$ad->image)); ?>" alt="">
+
+                        </a>
+                    </div>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+
+
+
+
+                </div>
+                <!-- /post content -->
+
+
+
+
+
+
+                <!-- /related post -->
+
+
+
+
+
+
+            </div>
+
+
+        </div>
+    </div>
+
+
+</div>
+<?php if(isset($horizontalAds[1]) == 1): ?>
+
+
+<div class="section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 section-row text-center">
+                <a target="_blank" href="<?php echo e($horizontalAds[1]->url); ?>" style="display: inline-block;margin: auto;">
+                    <img style="width: 728px; height: 90px" class="img-responsive"
+                        src="<?php echo e(url('storage/'.$horizontalAds[1]->image)); ?>" alt="">
+                </a>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<?php endif; ?>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.navbar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/resources/views/post-detail.blade.php ENDPATH**/ ?>
